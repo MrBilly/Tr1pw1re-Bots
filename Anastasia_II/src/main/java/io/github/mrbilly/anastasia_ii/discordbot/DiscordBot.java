02408b7dfba8b9e0764705748f25eec37385944e
MrBilly <mrbilly010@yahoo.com.au>
2016-06-23 20:21:53 +1000

    Update DiscordBot.java

--- Anastasia_II/src/main/java/io/github/mrbilly/anastasia_ii/discordbot/DiscordBot.java
+++ Anastasia_II/src/main/java/io/github/mrbilly/anastasia_ii/discordbot/DiscordBot.java
@@ -4,6 +4,7 @@ import javax.security.auth.login.LoginException;
 
 import net.dv8tion.jda.JDA;
 import net.dv8tion.jda.JDABuilder;
+import net.dv8tion.jda.entities.User;
 import net.dv8tion.jda.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.hooks.ListenerAdapter;
 
@@ -27,22 +28,26 @@ public class DiscordBot extends ListenerAdapter
     				+ "Cherry? Where did the Cherry go?\n"
     				+ "I'm a little hungry.");
     	}
-    	if (event.getMessage().getContent().equalsIgnoreCase("~anastasia info")) {
-            	event.getTextChannel().sendMessage(""
-            	+ "This bot is created by @cherry2003 and inspired by Discord/Billy :P");
-	}
-
-	if (event.getMessage().getContent().equalsIgnoreCase("~anastasia aye")) {
-            	event.getTextChannel().sendMessage(""
-            	+ "Aye there! I'm Anastasia!");
-	}
-	
-	if (event.getMessage().getContent().equalsIgnoreCase("~anastasia")) {
-		event.getTextChannel().sendMessage(""
-		+ "~anastasia = Displays this list\n"
-		+ "~anastasia info = Displays Bot Info\n"
-		+ "~anastasia aye = Say hello to Anastasia!\n"
-		+ "~anastasia cherry = Short message on Cherry... Random!");
-    	}
+    	
+        if (event.getMessage().getContent().toLowerCase().startsWith("~anastasia info")) {
+                String username = "cherry2003";
+                for (User user : event.getGuild().getUsers()) {
+                    if (user.getUsername().equalsIgnoreCase(username)) {
+                    	event.getTextChannel().sendMessage("This bot is created by " + user.getAsMention() + " and inspired by Discord/Billy :stuck_out_tongue:");
+                    }
+                }
+        }
+        
+        if (event.getMessage().getContent().equalsIgnoreCase("~anastasia aye")) {
+        	event.getTextChannel().sendMessage("Aye there! I'm " + event.getJDA().getSelfInfo().getAsMention() + "!");
+        }
+        
+        if (event.getMessage().getContent().equalsIgnoreCase("~anastasia")) {
+        	event.getTextChannel().sendMessage(""
+        			+ "~anastasia = Displays this list\n"
+        			+ "~anastasia info = Displays Bot Info\n"
+        			+ "~anastasia aye = Say hello to Anastasia!\n"
+        			+ "~anastasia cherry = Short message on Cherry... Random!");
+        }
     }
 }
