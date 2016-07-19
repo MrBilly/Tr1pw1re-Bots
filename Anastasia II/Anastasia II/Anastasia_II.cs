735d2fd21bef00304d58673eb9132ab070369e5b
MrBilly <mrbilly010@yahoo.com.au>
2016-07-19 20:50:30 +1000

    Anastasia II now in C#

---
+++ Anastasia II/Anastasia II/Anastasia_II.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+
+namespace Anastasia_II
+{
+    class Anastasia_II
+    {
+        static void Main(string[] args) => new Anastasia_II().Start();
+
+        public DiscordClient client;
+
+        public void Start()
+        {
+            client = new DiscordClient();
+
+            client.MessageReceived += Bot_Message;
+
+            Console.Title = "Anastasia II";
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Anastasia II"
+                + "\nWritten in C# with Discord.Net\n");
+            Console.ResetColor();
+
+            client.Log.Message += (s, e) => Console.WriteLine($"[{e.Severity}] {e.Source}: {e.Message}");
+
+            client.ExecuteAndWait(async () =>
+            {
+                await client.Connect("MTkxOTA2NTgwOTc4MDczNjA0.Ckwvcg.zrIDXUEyBrUF2rOt3CrImqYWzmA");
+            });
+        }
+
+        public void Bot_Message(object sender, MessageEventArgs e)
+        {
+            if (e.Message.IsAuthor) return;
+
+            Console.WriteLine("[" + DateTime.Now.ToString("hh:mm:ss | dd/MM/yyyy") + "] " + "< #{0} | {1} > {2}", e.Channel, e.User.Name, e.Message.Text);
+
+            DiscordCommands commandClass = new DiscordCommands();
+            commandClass.Bot_Message(sender, e);
+        }
+    }
+}
---
+++ Anastasia II/Anastasia II/DiscordCommands.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+
+namespace Anastasia_II
+{
+    class DiscordCommands
+    {
+        public DiscordClient client;
+
+        public async void Bot_Message(object sender, MessageEventArgs e)
+        {
+            client = new DiscordClient();
+
+            var Prefix = "~";
+
+            if (e.Message.IsAuthor) return;
+
+            if (e.Message.Text == Prefix + "anastasia cherry")
+            {
+                await e.Channel.SendMessage(""
+                    + "Cherry? Where did the Cherry go?\n"
+                    + "I'm a little hungry.");
+            }
+
+            if (e.Message.Text == Prefix + "anastasia info")
+            {
+                var cherry2003 = e.Server.FindUsers("cherry2003").FirstOrDefault();
+                await e.Channel.SendMessage(
+                    "This bot is created by " + cherry2003.Mention + " and inspirted by Discord/Billy :stuck_out_tongue:");
+            }
+
+            if (e.Message.Text == Prefix + "anastasia aye")
+            {
+                var bot = e.Server.FindUsers("Anastasia II").FirstOrDefault();
+                await e.Channel.SendMessage("Aye there! I'm " + bot.Mention + "!");
+            }
+
+            if (e.Message.Text == Prefix + "anastasia")
+            {
+                await e.Channel.SendMessage(""
+                    + "**`~anastasia`** = Displays this list\n"
+                    + "**`~anastasia info`** = Displays Bot Info\n"
+                    + "**`~anastasia aye`** = Say hello to Anastasia!\n"
+                    + "**`~anastasia cherry`** = Short message on Cherry... Random!");
+            }
+        }
+    }
+}
